101f576ecfe45760f214bf62663497fddf0b2c86
VctorMoraes <victorm.fernandes@outlook.com>
2024-10-11 16:46:54 -0300

    fix: remove unecessary param fromm AppLayout BottomNavComponent

--- src/components/home/AppLayout.tsx
+++ src/components/home/AppLayout.tsx
@@ -11,13 +11,12 @@ export default function AppLayout({children, showBottomNav}: AppLayoutProps) {
         <div className="flex flex-col h-screen">
             <div className="bg-sky-900 h-24 fixed top-0 left-0 right-0 z-10"/>
 
-            <div className={`mt-16 pt-8 flex-1 overflow-y-auto bg-white rounded-t-[40px] z-20 
-                ${showBottomNav ? 'pb-24' : 'pb-0'}`}>
-                    {children}
+            <div className={`mt-16 pt-8 flex-1 overflow-y-auto bg-white rounded-t-[40px] z-20`}>
+                {children}
             </div>
 
             {showBottomNav && (
-                <div className="fixed bottom-0 left-0 right-0 z-20">
+                <div className="bottom-0 left-0 right-0 z-30">
                     <BottomNavigation/>
                 </div>
             )}
