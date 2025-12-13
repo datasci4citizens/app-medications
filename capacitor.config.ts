2bb58ac37613531c93121b263bfd0bc316f8a3b3
Lorhan <gabriellorhan0@gmail.com>
2025-12-12 21:19:48 -0300

    Clean up main branch: remove deprecated code and unused files
    Refs #29

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-	appId: 'br.unicamp.ic.medications',
-	appName: 'medications',
-	webDir: 'dist',
-};
-
-export default config;
