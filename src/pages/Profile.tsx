a7fd4769beecd3b0dd4ac7e8d935b2ac33d4c950
Gabriel Lorhan <gabriellorhan0@gmail.com>
2026-03-27 13:54:36 -0300

    refactor: complete MVVM migration and remove legacy architecture

--- src/pages/Profile.tsx
+++
@@ -1,7 +0,0 @@
-export function Profile() {
-  return (
-    <div className="min-h-screen bg-graybg pb-20">
-      <p className="text-center mt-20 text-gray-400">Perfil em construção</p>
-    </div>
-  );
-}
