93be9fc99cf2292bbdbb4ce0e8a893352d408deb
Gabriel Lorhan <gabriellorhan0@gmail.com>
2026-03-30 02:23:05 -0300

    add Action Button Closes #77

---
+++ src/view/components/common/ActionButton.tsx
@@ -0,0 +1,32 @@
+
+interface ActionButtonProps {
+   label: string;
+   onClick: () => void;
+   variant: "success" | "warning" | "disabled" | "ghost"
+}
+
+
+export function ActionButton({ label, onClick, variant }: ActionButtonProps) {
+   const variantColor = {
+      success: "bg-green-take text-offwhite",
+      warning: "bg-yellow-alert text-inkblack",
+      disabled: "bg-ghost-gray text-",
+      ghost: "border-2 border-ghostcolor text-ghostcolor"
+   }
+
+   return (
+      <button className={`
+   w-full h-14 rounded-full
+   font-merriweather text-2xl
+   transform-bg
+   transition-all duration-200 ease-in-out
+   active:scale-90
+   active:brightness-75
+   ${variantColor[variant]}
+   `}
+      onClick={onClick}
+      
+      >
+         {label}
+      </button>);
+}
